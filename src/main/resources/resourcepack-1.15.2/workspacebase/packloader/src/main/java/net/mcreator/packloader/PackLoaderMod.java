cb91a87c3369cf59a489a1d00ba9614f43871880
Spectrall <105946758+Spectrall368@users.noreply.github.com>
2026-07-03 17:13:35 +0200

    Ported datapack & resourcepack

--- src/main/resources/resourcepack-1.15.2/workspacebase/packloader/src/main/java/net/mcreator/packloader/PackLoaderMod.java
+++ src/main/resources/resourcepack-1.15.2/workspacebase/packloader/src/main/java/net/mcreator/packloader/PackLoaderMod.java
@@ -21,8 +21,7 @@ public class PackLoaderMod {
 
     public static final String MODID = "packloader";
 
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT) public static class ClientModEvents {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
             List<String> resourcePacks = new ArrayList<>();
